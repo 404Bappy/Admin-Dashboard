e450e8e3aec6e06f0f20960df288af7c7aeb1598
Bappy71 <19103076@iubat.edu>
2022-11-01 23:44:38 +0600

    initial comit

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ const closeBtn = document.getElementById("close-btn");
 menuBtn.addEventListener('click', ()=>{
     sideMenu.style.display = 'block';
 })
+
+closeBtn.addEventListener('click', ()=>{
+    sideMenu.style.display = 'none';
+})
