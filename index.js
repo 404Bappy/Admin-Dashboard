70ec81ad44bca2db8bfb1e40f4b0ff6a29835840
Bappy71 <19103076@iubat.edu>
2022-11-02 21:12:11 +0600

    initial comit

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const sideMenu = document.querySelector("aside");
 const menuBtn = document.getElementById("menu-btn");
 const closeBtn = document.getElementById("close-btn");
 const themeToggler = document.querySelector(".theme-toggler");
-
+/// =========> Show Sidebar =======>>>
 menuBtn.addEventListener('click', () => {
     sideMenu.style.display = 'block';
 })
-
+// ==========> Close Sidebar ========>>>>
 closeBtn.addEventListener('click', () => {
     sideMenu.style.display = 'none';
 })
 
-//==========>Change Theme========>
+//==========>Change Theme========>>>
 themeToggler.addEventListener('click', () => {
     document.body.classList.toggle('dark-theme-variables');
 })
